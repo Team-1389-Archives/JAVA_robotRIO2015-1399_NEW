c0e051396de405dd589158b2cee32e5651d87b11
Paul LoBuglio <paullo101123@gmail.com>
2015-01-31 15:39:05 -0500

    auton 3 complete

--- org/usfirst/frc/team1389/robot/Autonomous.java
+++ org/usfirst/frc/team1389/robot/Autonomous.java
@@ -12,6 +12,8 @@ public class Autonomous {
 	private final double STAGING_ZONE_WIDTH=48;//length down the field of yellow crate zone
 	private final double STAGING_ZONE_LENGTH=9;//width of yellow crate zone
 	private final double BETW_AUTO_TOTES = 33; //Distance to travel inbetween auto totes when picking up all totes
+	private final double TOTE_WIDTH = 26.9; //width of a tote
+	
 	private DriveControl drive;
 	private ElevatorControl elevator;
 	private PosTrack pos;
@@ -52,7 +54,13 @@ public class Autonomous {
 
 	public void autonThree()
 	{
-
+		elevator.goTo(4);
+		drive.move(TOTE_WIDTH, AUTON_SPEED_MOD/1.2);
+		elevator.goTo(0);
+		elevator.goTo(1);
+		drive.turn(-90);
+		drive.move(TAPE_TO_LANDMARK, AUTON_SPEED_MOD);
+		
 	}
 
 	public void autonFour()
