fb0843b70567fa91e7e1c06b55518debf996b541
unknown <336450@427-D218-12-088.mcpsmd.org>
2015-01-31 12:54:21 -0500

    Set up Autonomous framework

---
+++ org/usfirst/frc/team1389/robot/Autonomous.java
@@ -0,0 +1,57 @@
+package org.usfirst.frc.team1389.robot;
+
+public class Autonomous {
+
+	public Autonomous(int methodNum)
+	{
+		switch(methodNum)
+		{
+		case 1: autonOne(); break;
+		case 2: autonTwo(); break;
+		case 3: autonThree(); break;
+		case 4: autonFour(); break;
+		case 5: autonFive(); break;
+		case 6: autonSix(); break;
+		case 7: autonSeven(); break;
+		default: break;
+
+		}
+	}
+
+	public void autonOne()
+	{
+
+	}
+
+	public void autonTwo()
+	{
+
+	}
+
+	public void autonThree()
+	{
+
+	}
+
+	public void autonFour()
+	{
+
+	}
+
+	public void autonFive()
+	{
+
+	}
+
+	public void autonSix()
+	{
+
+	}
+
+	public void autonSeven()
+	{
+
+	}
+
+
+}
