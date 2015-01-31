70ad7fa02c272a3fbf9691899b1aa30b25158539
Paul LoBuglio <paullo101123@gmail.com>
2015-01-31 14:21:19 -0500

    added component access to autonomous class

--- org/usfirst/frc/team1389/robot/Autonomous.java
+++ org/usfirst/frc/team1389/robot/Autonomous.java
@@ -1,15 +1,23 @@
 package org.usfirst.frc.team1389.robot;
 
+import java.util.ArrayList;
+
 public class Autonomous {
 	
 	//These constants hold relevant distances we need to travel in inches
-	static final float STAG_TO_AUTO = 80; //Distance from in front of AutoTotes -> AutoZone.
-	static final float BETW_AUTO_TOTES = 85; //Distance to travel inbetween auto totes when picking up all totes
+	private final double MULTIPLIER=0.0254*16;//inches->meters conversion * 1/16 scale model conversion
+	private final double STAG_TO_AUTO = 80; //Distance from in front of AutoTotes -> AutoZone.
+	private final double BETW_AUTO_TOTES = 85; //Distance to travel inbetween auto totes when picking up all totes
 	
+	private DriveControl drive;
+	private ElevatorControl elevator;
+	private PosTrack pos;
 	
-	
-	public Autonomous(int methodNum)
+	public Autonomous(int methodNum,ArrayList<Component> components)
 	{
+		pos=(PosTrack)components.get(Robot.POS);
+		drive=(DriveControl)components.get(Robot.DRIVE);
+		elevator=(ElevatorControl)components.get(Robot.ELEVATOR);
 		switch(methodNum)
 		{
 		case 1: autonOne(); break;
@@ -20,17 +28,17 @@ public class Autonomous {
 		case 6: autonSix(); break;
 		case 7: autonSeven(); break;
 		default: break;
-
 		}
 	}
 
 	public void autonOne()
 	{
+		
 	}
 
 	public void autonTwo()
 	{
-
+		
 	}
 
 	public void autonThree()
